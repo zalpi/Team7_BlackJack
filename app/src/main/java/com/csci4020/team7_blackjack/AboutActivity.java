9620c93f91a044afdc4eae36361a637fab7df25e
Zachary Pigott <zach.pigott@gmail.com>
2017-02-06 10:39:08 -0600

    Added an extend.

--- app/src/main/java/com/csci4020/team7_blackjack/AboutActivity.java
+++ app/src/main/java/com/csci4020/team7_blackjack/AboutActivity.java
@@ -1,8 +1,11 @@
 package com.csci4020.team7_blackjack;
 
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by Pigott on 2/6/2017.
  */
 
-public class AboutActivity {
+public class AboutActivity extends AppCompatActivity {
+
 }
