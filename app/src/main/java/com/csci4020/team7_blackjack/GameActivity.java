fcb34e010320ebadc0b30f1c034d8a1efb01b9f4
Zachary Pigott <zach.pigott@gmail.com>
2017-02-06 09:34:48 -0600

    Created GameActivity.class and updated the manifest. Changed the button ids for activity_main.xml.

---
+++ app/src/main/java/com/csci4020/team7_blackjack/GameActivity.java
@@ -0,0 +1,11 @@
+package com.csci4020.team7_blackjack;
+
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+/**
+ * Created by Pigott on 2/6/2017.
+ */
+
+public class GameActivity extends AppCompatActivity implements View.OnClickListener {
+}
