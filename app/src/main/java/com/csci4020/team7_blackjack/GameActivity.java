d5d3e7d86d6cf5c705641ab7fb27fe15504e33bd
Zachary Pigott <zach.pigott@gmail.com>
2017-02-06 11:02:55 -0600

    Having rendering issues with the layouts, tried to make the hit/stand buttons align.

--- app/src/main/java/com/csci4020/team7_blackjack/GameActivity.java
+++ app/src/main/java/com/csci4020/team7_blackjack/GameActivity.java
@@ -9,6 +9,12 @@ import android.view.View;
  * Created by Pigott on 2/6/2017.
  */
 
+//TODO set up scoring rules.
+//TODO set up a YOU WON/YOU LOST dialog.
+//TODO set up card counting to prevent duplicates.
+//TODO set up dealer switch cases.
+//TODO add money to bet along with an amount option.
+
 public class GameActivity extends AppCompatActivity implements View.OnClickListener {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
